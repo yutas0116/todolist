0ef2f792836e8396b02e2902196224d4c27a766f
yutas0116 <yutanpo0116@ezweb.ne.jp>
2021-10-08 23:06:45 +0900

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by 菅島優太 on 2021/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by 菅島優太 on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
